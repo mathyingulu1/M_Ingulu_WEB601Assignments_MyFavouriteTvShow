56c82817d2dcec51b5369b412aea4378d283965e
mathyingulu1 <90633640+mathyingulu1@users.noreply.github.com>
2022-02-05 00:09:46 -0500

    just correct my content list file

--- M_Ingulu_MyFavoriteTvShow-angular/src/app/helper-files/content-list.ts
+++ M_Ingulu_MyFavoriteTvShow-angular/src/app/helper-files/content-list.ts
@@ -3,26 +3,21 @@ import{ Content } from "./content-interface";
 export class Contentlist {
     private _items: Content[];
 
-    constructor() {
-        this._items = {
-        //this._items[0] = item;
-        //items = {
-          id: 1,
-          title: "24",
-          description: "American drama series",
-          cretor: "Joel Surnow",
-          type: "Action",
-          imageUrl: "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?202201181338", 
-        };
-    }
-    
-
-get items(): Content[] {
+    constructor(item:Content) {
+        this._items = [];
+         }
+    get items(): Content[] {
     return this._items;
-}
+    }
+    addItem(item: Content) {
+        this._items.push(item);
+    }
+    contentLength():number {
+        return this._items.length;
+    }
 }
 
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
     selector: 'Content-card',
@@ -31,7 +26,7 @@ import { Component } from '@angular/core';
   })
 
   export class AppComponent {
-    title: string;
+    title: 'M_Ingulu_MyFavoriteTvshow';
 
     constructor() {
         this.title = '<em>ContentList</em>';
@@ -54,4 +49,4 @@ import { Component } from '@angular/core';
             cast: 'Hank Azaria'
         };
     }
-}
+}*/
