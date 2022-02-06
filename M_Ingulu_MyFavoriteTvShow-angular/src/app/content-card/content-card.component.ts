9f00329ccf9f9e217f150930c0cd73ff29a31ee0
mathyingulu1 <90633640+mathyingulu1@users.noreply.github.com>
2022-02-05 23:26:01 -0500

    content card modification

--- M_Ingulu_MyFavoriteTvShow-angular/src/app/content-card/content-card.component.ts
+++ M_Ingulu_MyFavoriteTvShow-angular/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contentlist} from "../helper-files/content-list";
 
 @Component({
   selector: 'app-content-card',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
+  myList: Contentlist;
 
-  constructor() { }
+  constructor(){
+    this.myList = new Contentlist();
+    console.log("my list contains these items: ", this.myList.items);
+
+    this.myList.addItem({
+      id: 0,
+      title: "Prison Break",
+      description: "Tv show",
+      creator: "Paul Scheuring",
+      imgURL: "https://en.wikipedia.org/wiki/Prison_Break#/media/File:Prison-break-s1-intro.jpg"
+  });
+
+  this.myList.addItem({
+    id: 1,
+    title: "One Tree Hill",
+    description: "Teenager show",
+    creator: "Mark Schwahn",
+    imgURL: "https://en.wikipedia.org/wiki/One_Tree_Hill_(season_4)#/media/File:One_Tree_Hill_-_Season_4_-_DVD.JPG",
+    type: "drama"
+  });
+
+  this.myList.addItem({
+    id: 2,
+    title: "24 hours",
+    description: "Tv show",
+    creator: "Joel Surnow",
+    imgURL: "https://en.wikipedia.org/wiki/24_(TV_series)#/media/File:24-Logo.svg",
+    type: "american drama"
+  });
+
+  console.log("my list contains these items: ", this.myList.items);
+
+
+  console.log("The length of the array is:  ", this.myList.numberOfItems());
+
+  console.log("THe html output is: ", this.myList.getHtml(0));
+
+  // myList.items = [];
+}
 
   ngOnInit(): void {
   }
-
 }
--- M_Ingulu_MyFavoriteTvShow-angular/src/app/helper-files/content-list.ts
+++ M_Ingulu_MyFavoriteTvShow-angular/src/app/helper-files/content-list.ts
@@ -3,7 +3,7 @@ import{ Content } from "./content-interface";
 export class Contentlist {
     private _items: Content[];
 
-    constructor(item:Content) {
+    constructor() {
         this._items = [];
          }
     get items(): Content[] {
@@ -12,10 +12,27 @@ export class Contentlist {
     addItem(item: Content) {
         this._items.push(item);
     }
+
+    numberOfItems(): number{
+        return this._items.length;
+      }
     contentLength():number {
         return this._items.length;
     }
+    getHtml(index: number): string{
+        if (index >= this._items.length || index < 0)
+        {
+          return "<div>Something went terribly wrong</div>"
+        }
+        let itemAtIndex = this._items[index]
+        return `<div class="title">${itemAtIndex.title}</div>
+                <div class="description">${itemAtIndex.description}</div>
+                <div class="creator">${itemAtIndex.creator}</div>
+                <div class="image"><img src="${itemAtIndex.imgURL}" width="200"></div>
+                <div class="type">${itemAtIndex.type}</div>`;
 }
+}
+
 
 /*import { Component } from '@angular/core';
 
