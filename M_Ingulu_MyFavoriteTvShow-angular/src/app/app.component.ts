e04ad0670fa396a24456ef83ae73f286308ce0b1
mathyingulu1 <90633640+mathyingulu1@users.noreply.github.com>
2022-02-12 23:55:12 -0500

    create highlight pipe

--- M_Ingulu_MyFavoriteTvShow-angular/src/app/app.component.ts
+++ M_Ingulu_MyFavoriteTvShow-angular/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M_Ingulu_MyFavoriteTvShow-angular';
+
+  show= {
+    title: 'Revenge',
+    creator: 'Mike Kelley',
+    genre: 'Drama'
+  };
 }
---
+++ M_Ingulu_MyFavoriteTvShow-angular/src/app/highlight.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'highlight'
+})
+export class HighlightPipe implements PipeTransform {
+
+  transform(value: string, title: string): string {
+    return 'show title';
+  }
+
+}
